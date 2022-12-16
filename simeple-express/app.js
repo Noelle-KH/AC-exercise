d08bc1ef7cfe304bf7f8c9f78d8693534b54bfbf
Noelle-KH <noelletang86@gmail.com>
2022-12-16 21:22:21 +0800

    feat: add three page routing and views

--- simeple-express/app.js
+++ simeple-express/app.js
@@ -3,12 +3,36 @@ const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
-app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.engine('handlebars', exphbs.engine({
+  defaultLayout: 'main',
+  helpers: {
+    active: (active) => {
+      if (active) {
+        return 'active'
+      }
+    }
+  }
+}))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/about', (req, res) => {
+  const about = true
+  res.render('about', { about })
+})
+
+app.get('/portfolio', (req, res) => {
+  const portfolio = true
+  res.render('portfolio', { portfolio })
+})
+
+app.get('/contact', (req, res) => {
+  const contact = true
+  res.render('contact', { contact })
 })
 
 app.listen(port, () => {
