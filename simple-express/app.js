bfda03ad30b50dc27c41eb26e0c45c11002a4dda
Noelle-KH <noelletang86@gmail.com>
2022-12-29 15:29:38 +0800

    feat: add error page function

--- simple-express/app.js
+++ simple-express/app.js
@@ -23,9 +23,14 @@ app.get('/', (req, res) => {
 
 app.get('/:page', (req, res) => {
   const page = req.params.page
-  const title = page[0].toUpperCase() + page.slice(1)
-  const active = { [page]: true }
-  res.render('index', { title, active })
+  if (page === 'about' || page === 'portfolio' || page === 'contact') {
+    const title = page[0].toUpperCase() + page.slice(1)
+    const active = { [page]: true }
+    res.render('index', { title, active })
+  } else {
+    const error = 'Error Page'
+    res.render('index', { error })
+  }
 })
 
 app.listen(port, () => {
